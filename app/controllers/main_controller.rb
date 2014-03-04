afaf3f3b12634f1d7cab08aca784fa8d278b1894
Dmitriy <dmitrypavlov1997@gmail.com>
2014-03-04 18:37:57 +1000

    fixed last bugs

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
 	has_mobile_fu 
+    before_filter :force_mobile_format
 	def index
 		if params["submit"] != nil and cookies[:posted] == nil
 				@new_message = Message.new(params[:message].permit(:name, :text))
